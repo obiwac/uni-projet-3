efe2cc0ef3a7a6252ba6215ae7a40e20eb85bc53
Lucas Rota-Benabid <lucas.rota.benabid@gmail.com>
2021-12-07 12:43:15 +0100

    Added time to main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import module.list
 import module.flashlight
+import module.time
 
 shopping_list = list.List(rhasspy)
 flashy_boi = flashlight.Flashlight()
+the_time = time.Time()
 
 import rhasspy
 rhasspy.train_intent_files("sentences.ini")
@@ -15,3 +17,4 @@ while True:
 
 	shopping_list.process(action, params)
 	flashy_boi.process(action)
+	the_time.process(action)
