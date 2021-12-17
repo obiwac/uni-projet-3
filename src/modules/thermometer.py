631139008058bbe0f1c0869b104f7a31cf784421
obiwac <obiwac@gmail.com>
2021-12-17 17:20:21 +0100

    docstrings: `modules/thermometer.py`

--- src/modules/thermometer.py
+++ src/modules/thermometer.py
@@ -7,6 +7,12 @@ class Thermometer(module.Module):
 		super().__init__()
 	
 	def temperature_from_pressure(self):
+		"""
+		Get temperature from pressure sensor.
+		Show the degrees celsius on screen and say it out loud.
+		If it's too hot or too cold, notify the user.
+		"""
+
 		temp = graphics.sense.get_temperature()
 		graphics.text(str(int(round(temp))))
 		self.say(f"Il fait {round(temp)} degrés celsius")
@@ -18,5 +24,9 @@ class Thermometer(module.Module):
 			self.say("Il fait froid, buvez quelque chose de chaud")
 
 	def process(self, action, params):
+		"""
+		Process potential thermometer commands.
+		"""
+
 		if action == "Tell_temperature":
 			self.temperature_from_pressure()
